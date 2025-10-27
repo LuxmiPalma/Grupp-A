0198e4afd866cb379ca0b7875b3d7c60f7a601f0
Nelly <Nelly.sosobrado@gmail.com>
2025-10-27 13:18:43 +0100

    Added folders with test files

--- CoreGymBookingSystem/DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
+++ CoreGymBookingSystem/DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c3b52188c059c0e37f9278eed012d43890600de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08a2c56602868243c25daa6d1a51e23af6ec7c95")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
