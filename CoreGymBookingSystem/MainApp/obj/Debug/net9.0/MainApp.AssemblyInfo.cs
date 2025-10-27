df296f3d6548550e545dd54b3def0b4903d2610f
Mustafee <mussemoh03@gmail.com>
2025-10-27 11:37:04 +0100

    test

--- CoreGymBookingSystem/MainApp/obj/Debug/net9.0/MainApp.AssemblyInfo.cs
+++ CoreGymBookingSystem/MainApp/obj/Debug/net9.0/MainApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913075bfc1feb072bc6a9470675ef3e5f66ccf14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb203a9bcd399c8d04dd248fa3c724135fa44eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
