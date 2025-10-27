f3cc666c19a8e8ddaffe3c37011a89b9272c1525
Mustafee <mussemoh03@gmail.com>
2025-10-27 14:05:21 +0100

    created services and interfaces

--- CoreGymBookingSystem/Service/obj/Debug/net9.0/Service.AssemblyInfo.cs
+++ CoreGymBookingSystem/Service/obj/Debug/net9.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c3b52188c059c0e37f9278eed012d43890600de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0198e4afd866cb379ca0b7875b3d7c60f7a601f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
