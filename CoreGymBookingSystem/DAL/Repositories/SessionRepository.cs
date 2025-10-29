a7df3d0a71475c8c43ca391cdfe590ceaba1357c
LuxmiPalma <luxmi.palma@gmail.com>
2025-10-29 21:21:00 +0100

    Add Session entity and related services
    Introduced a new `Session` entity to manage session data. Updated `ApplicationDbContext` and added a `SeedSessions`. Registered `ISessionRepository` and `ISessionService` in `Program.cs`. Created migration files to add `Sessions` table and `DayOfWeek` column. Implemented `SessionRepository` and `SessionService` for data access and business logic.

---
+++ CoreGymBookingSystem/DAL/Repositories/SessionRepository.cs
@@ -0,0 +1,40 @@
+﻿using DAL.DbContext;
+using DAL.Entitites;
+using DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories;
+
+public class SessionRepository : ISessionRepository
+
+{
+    private readonly ApplicationDbContext _context;
+
+    public SessionRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Session>> GetAllAsync()
+    {
+        return await _context.Sessions.Include(s => s.Instructor).ToListAsync();
+    }
+
+    public async Task<Session?> GetByIdAsync(int id)
+    {
+        return await _context.Sessions
+            .Include(s => s.Instructor)
+            .FirstOrDefaultAsync(s => s.Id == id);
+    }
+
+    public async Task AddAsync(Session session)
+    {
+        await _context.Sessions.AddAsync(session);
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+}
