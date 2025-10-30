3d5352fd3b863b774bc5862e18d028cded54ddb7
Nelly <Nelly.sosobrado@gmail.com>
2025-10-30 08:37:10 +0100

    Connected Account dashboard when clicking on view to display the session details.
    Updated Dashboard Page to english

---
+++ CoreGymBookingSystem/MainApp/Pages/Admin/Sessions/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MainApp.Pages.Admin.Sessions
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
