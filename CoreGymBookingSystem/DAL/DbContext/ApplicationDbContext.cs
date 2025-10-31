2e7924e501b4d9a7c4ca7ad3bbba928f459dfcb3
Eleonora Alminson <ellialminson@me.com>
2025-10-31 13:02:27 +0100

    Updated namespaces, and refactor Session from Entitites to new Entities folder
    Removed Test.cs empty class from Entitites folder.
    Updated target framework to net9.0 in DAL.csproj, enabled implicit usings and nullable reference types, and added necessary package references. Corrected namespace from DAL.Entitites to DAL.Entities across multiple files. Refactored Session.cs with updated class definition. Removed unused imports in _ViewImports.cshtml and Program.cs.

--- CoreGymBookingSystem/DAL/DbContext/ApplicationDbContext.cs
+++ CoreGymBookingSystem/DAL/DbContext/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using DAL.Entitites;
+﻿using DAL.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
--- CoreGymBookingSystem/DAL/Entities/Session.cs
+++ CoreGymBookingSystem/DAL/Entities/Session.cs
@@ -1,9 +1,7 @@
-﻿
-
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace DAL.Entitites;
+namespace DAL.Entities;
 
 public class Session
 {
