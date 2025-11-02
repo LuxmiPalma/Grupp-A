ed63e7a30393c39ed5e7960023a54f66ad18ad48
Nelly <Nelly.sosobrado@gmail.com>
2025-11-02 20:53:45 +0100

    Fixed error : ""user" could not be found ..".
    Added correct reference to entities where "user" class is located.

--- CoreGymBookingSystem/MainApp/Areas/Identity/Pages/Account/Manage/GenerateRecoveryCodes.cshtml.cs
+++ CoreGymBookingSystem/MainApp/Areas/Identity/Pages/Account/Manage/GenerateRecoveryCodes.cshtml.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using DAL.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
