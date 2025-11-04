9a973f3bf10fa19f05320c81e2e1e7aa9c676786
Mustafee <mussemoh03@gmail.com>
2025-11-04 19:18:19 +0100

    finished searching category

---
+++ CoreGymBookingSystem/MainApp/ViewModel/SessionViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MainApp.ViewModel
+{
+    public class SessionViewModel
+    {
+        public string Title { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Summary of the activities.
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+
+        /// <summary>
+        /// Summary of the activities.
+        /// </summary>
+        public string Category { get; set; } = string.Empty;
+    }
+}
